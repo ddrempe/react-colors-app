dd1a44b126e4cd5cb3925eeb669aaa9e797f479e
Damir <damir.drempetic@live.com>
2021-02-09 13:06:39 +0100

    Added input for button text

--- src/components/Colors.jsx
+++ src/components/Colors.jsx
@@ -1,6 +1,7 @@
 import { Fragment, useState } from "react";
 import axios from "axios";
 import Button from "components/Button";
+import Input from "components/Input";
 
 const getRandomColorFromApi = async () => {
   const response = await axios.get(
@@ -14,18 +15,29 @@ const getRandomColorFromApi = async () => {
 };
 
 export default function Colors(props) {
-  const [currentColor, setCurrentColor] = useState("B22222");
+  const [buttonText, setButtonText] = useState("Get random color");
+  const [currentColor, setCurrentColor] = useState("b22222");
+  const [colorHistory, setColorHistory] = useState([]);
 
   const handleOnClick = async () => {
     const newColor = await getRandomColorFromApi();
     setCurrentColor(newColor);
+    setColorHistory([currentColor, ...colorHistory]);
   };
+
   return (
     <Fragment>
-      <Button
-        value={"Get random color"}
-        color={currentColor}
-        onClick={handleOnClick}
+      <Button value={buttonText} color={currentColor} onClick={handleOnClick} />
+
+      {colorHistory.map((color, ix) => (
+        <div key={ix}>#{color}</div>
+      ))}
+
+      <Input
+        value={buttonText}
+        onChange={(event) => {
+          setButtonText(event.target.value);
+        }}
       />
     </Fragment>
   );
---
+++ src/components/Input.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  border-radius: 3px;
+  height: 32px;
+  width: 200px;
+  border: none;
+  text-align: center;
+  font-size: 16px;
+  font-weight: bold;
+  margin: 8px;
+`;
+
+export default function Input(props) {
+  const { ...rest } = props;
+
+  return <StyledInput type="text" {...rest} />;
+}
